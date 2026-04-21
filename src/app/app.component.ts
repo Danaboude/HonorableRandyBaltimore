20b141dbdd78ec6abd99330ecdd193390c46b44c
danaboude <danaboude@gmail.com>
2026-04-21 12:13:38 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
 import { News } from './components/news/news';
+import { Election } from './components/election/election';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero, About, News, Footer],
+  imports: [RouterOutlet, Navbar, Hero, About, News, Election, Footer],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
---
+++ src/app/components/election/election.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ScrollAnimateDirective } from '../../directives/scroll-animate';
+
+@Component({
+  selector: 'app-election',
+  imports: [ScrollAnimateDirective],
+  templateUrl: './election.html',
+  styleUrl: './election.css'
+})
+export class Election {
+
+}
