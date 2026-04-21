e75a42899f96660b3345534b7219d7182afd01e5
danaboude <danaboude@gmail.com>
2026-04-21 12:20:26 -0400

    updates

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -8,9 +8,18 @@ import { Component, HostListener } from '@angular/core';
 })
 export class Navbar {
   isScrolled = false;
+  isMobileMenuOpen = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 20;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
   }
 }
